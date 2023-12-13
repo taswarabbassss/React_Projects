6fecb1e0c8cfde9dc2a2045796eedebc22cd1c09
Tasawar Abbas <taswarabbassss@gmail.com>
2023-12-13 21:43:44 +0500

    Added NavBar with NavLink

--- vite/worldwise/src/pages/Homepage.jsx
+++ vite/worldwise/src/pages/Homepage.jsx
@@ -1,5 +1,11 @@
+import PageNav from "./PageNav";
+
 function Homepage() {
-  return <div>World Wise</div>;
+  return (
+    <div>
+      <PageNav /> <h1>World Wise</h1>
+    </div>
+  );
 }
 
 export default Homepage;
