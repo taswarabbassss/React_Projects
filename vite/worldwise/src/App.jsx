ea00cd4c36f60f4f0c77898af8ca10e55771ab44
Tasawar Abbas <taswarabbassss@gmail.com>
2023-12-19 23:46:16 +0500

    Completed the City Component

--- vite/worldwise/src/App.jsx
+++ vite/worldwise/src/App.jsx
@@ -5,8 +5,33 @@ import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import { useState } from "react";
+import { useEffect } from "react";
+import CityList from "../components/CityList";
 
+const BASE_URL = "http://localhost:8000";
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch {
+        alert("There was an error loading data.");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,8 +40,14 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>Default Cities</p>} />
-          <Route path="cities" element={<p>List of Cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
