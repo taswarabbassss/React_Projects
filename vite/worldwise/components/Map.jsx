8cbb91d451f5f22e3ab2ef30c7ec31ea514e7955
Tasawar Abbas <taswarabbassss@gmail.com>
2023-12-20 22:48:31 +0500

    useParam and useSearchParam Hook

--- vite/worldwise/components/Map.jsx
+++ vite/worldwise/components/Map.jsx
@@ -1,6 +1,17 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 function Map() {
-  return <div className={styles.mapContainer}>MAP</div>;
+  const [searchParam, setSearchParam] = useSearchParams();
+  const lat = searchParam.get("lat");
+  const lng = searchParam.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      <h1>MAP</h1>
+      <h1>
+        Position: {lat},{lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
